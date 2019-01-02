30c49a61f639528a22eb1d588f86064f95b21bdd
Eduard <xsynergy510x@gmail.com>
2019-01-01 22:05:49 -0800

    Add push noti if received not enough args

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from teslaApi import *
 # Get and Verify Command Argument
 if len(sys.argv) < 2:
     print("No command specified!\n Usage: python3 main.py cmd data\n")
+    joinPush("TeslaAPI Failure", sys.argv[1] + sys.argv[2])
     exit()
 
 title, text = apiAccess(sys.argv[1], sys.argv[2])
